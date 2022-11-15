cb3f2d0526e6489f2aec6b19cf8bd990eecd0bbd
Madhumitha-Rathinasamy <madhumitha@sparkouttech.com>
2022-11-15 15:29:41 +0530

    CrowdSale

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,9 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
-const SparkTokens = artifacts.require("ERC20Upgradeable");
+const crowdSale = artifacts.require("crowdSale");
 
 module.exports = async function(deployer) {
 
-    await deployProxy(SparkTokens,{ deployer, kind: "uups" });
+    await deployProxy(crowdSale,{ deployer, kind: "uups" });
     // await upgradeProxy("0x5Ffb1d89c92B3fD221254201Fb7E175751a39Ba5", SparkTokens, { deployer, kind: "uups" });
 };
